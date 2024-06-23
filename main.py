cc8072f361428232820e59b03cd90ab334c6f712
ahmed5145 <ahmedmohamed200354@gmail.com>
2024-06-23 18:06:33 -0500

    Bug fixes and reformations

--- main.py
+++ main.py
@@ -1,31 +1,43 @@
 import os
 import numpy as np
 import pickle
-
-from skimage.io import imread
-from skimage.transform import resize
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+from skimage.io import imread
+from skimage.transform import resize
+from sklearn.preprocessing import StandardScaler
 
 # Prepare data
 input_dir = 'C:\\Users\\hussah01\\Desktop\\CS Projects\\Image Classification Computer Vision\\Chess'
 categories = ['Bishop', 'King', 'Knight', 'Pawn', 'Queen', 'Rook']
 data = []
 labels = []
+target_shape = (64, 64)  # Target image resolution
+
+# Supported image extensions
+supported_extensions = ('.jpg', '.jpeg', '.png')
+
 for category_idx, category in enumerate(categories):
-    for file in os.listdir(os.path.join(input_dir,category)):
-        img_path = os.path.join(input_dir, category, file)
-        img = imread(img_path)
-        img = resize(img, (15, 15, img.shape[2]))
-        data.append(img.flatten())
-        labels.append(category_idx)
-        
-data = np.asarray(data)
-labels = np.asarray(labels)
+    category_path = os.path.join(input_dir, category)
+    for file in os.listdir(category_path):
+        if file.lower().endswith(supported_extensions):  # Check for supported image extensions
+            img_path = os.path.join(category_path, file)
+            img = imread(img_path)
+            img = resize(img, target_shape)  # Resize image
+            img = img.astype(np.float32)  # Convert to float32
+            data.append(img.flatten())
+            labels.append(category_idx)
+
+data_array = np.array(data)
+labels_array = np.array(labels)
+
+# Normalize data
+scaler = StandardScaler()
+data_array = scaler.fit_transform(data_array)
 
 # Train / Test Split
-x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+x_train, x_test, y_train, y_test = train_test_split(data_array, labels_array, test_size=0.2, shuffle=True, stratify=labels_array)
 
 # Train classifier
 classifier = SVC()
@@ -42,6 +54,7 @@ y_prediction = best_estimator.predict(x_test)
 
 score = accuracy_score(y_prediction, y_test)
 
-print('{}% of samles were correctly classified'.format(str(score*100)))
+print(f'{score*100:.2f}% of samples were correctly classified')
 
+# Save the model
 pickle.dump(best_estimator, open('./model.p', 'wb'))
